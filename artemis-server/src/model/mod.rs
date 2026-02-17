bd4076305e4a39d1e8bdec55570d516dbbbe180b
Qiang Zhao <koqizhao@outlook.com>
2026-02-17 14:05:37 +0800

    chore: 创建 artemis-server 新模块目录结构

---
+++ artemis-server/src/model/mod.rs
@@ -0,0 +1,7 @@
+//! Server-specific data models
+
+pub mod lease;
+pub mod replication;
+
+pub use lease::Lease;
+pub use replication::*;
---
+++ artemis-server/src/traits/mod.rs
@@ -0,0 +1,7 @@
+//! Service traits for server business logic
+
+pub mod discovery;
+pub mod registry;
+
+pub use discovery::DiscoveryService;
+pub use registry::RegistryService;
