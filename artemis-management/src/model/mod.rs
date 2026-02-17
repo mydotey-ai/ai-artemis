29a96da18e633f864b8ce1aaaab371d6d6dd6eb0
Qiang Zhao <koqizhao@outlook.com>
2026-02-17 15:19:44 +0800

    chore: 创建 artemis-management 新模块目录结构

---
+++ artemis-management/src/model/mod.rs
@@ -0,0 +1,15 @@
+//! Management-specific data models
+
+pub mod canary;
+pub mod group;
+pub mod management;
+pub mod route;
+pub mod status;
+pub mod zone;
+
+pub use canary::*;
+pub use group::*;
+pub use management::*;
+pub use route::*;
+pub use status::*;
+pub use zone::*;
